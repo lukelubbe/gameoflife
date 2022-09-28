b9ff96993825d16054b469e87a6c9cf15a9d0cca
luke.lubbe <luke.lubbe@entelect.co.za>
2022-09-28 18:23:00 +0200

    test

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,14 @@
+fun main(args : Array<String>) {
+    println("Kotlin is interesting.")
+    val size = readLine()!!
+    val startx = readLine()!!
+    val starty = readLine()!!
+
+
+    println(size+ " "+ startx+" "+ starty)
+
+    val array = Array(Integer.parseInt(size)) { IntArray(Integer.parseInt(size))}
+
+
+
+}
